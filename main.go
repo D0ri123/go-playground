74dc64635546a0824fef3bcd5dd4dabc21a33c1d
D0ri123 <lsh81550@gmail.com>
2023-12-24 20:27:56 +0900

    fmt 패키지 사용 여부 차이

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello goorm!")
+	var num1 int = 1
+	var num2 int = 2
+
+	//fmt 패키지를 사용하지 않는 경우
+	print("Hello goorm!")
+	print(num1)
+	print(num2)
+	print(num1 + num2)
+	print("Hello goorm!", num1+num2, "\n")
+
+	println("Hello goorm!")
+	println(num1)
+	println(num2)
+	println(num1 + num2)
+	println("Hello goorm!", num1+num2)
+
+	//fmt 패키지를 사용하는 경우
+	fmt.Print("Hello goorm!", num1, num2, "\n")
+
+	fmt.Println("Hello goorm!", num1, num2)
+
+	fmt.Printf("num1의 값은:%d num2의 값은:%d\n", num1, num2)
 }
