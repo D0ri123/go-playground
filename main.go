2d495cf32ac0997e5c701ddbf9978f45c0685372
D0ri123 <lsh81550@gmail.com>
2023-12-26 15:08:32 +0900

    콘솔 입력 함수

--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	age, name := 24, "길동"
+	var name string
+	var gen string
+	var school string
 
-	fmt.Printf("안녕 난 %s이야\n", name)
-	fmt.Printf("나이는 %d살이야\n", age)
-	fmt.Printf("반가워")
+	fmt.Print("이름, 성별, 학교를 입력하세요.")
 
-	n := 14
-	var arr [5]int = [5]int{1, 2, 3, 4, 5}
+	/**
+	공백으로 구분하여 입력한다.
+	*/
+	//fmt.Scanln(&name, &gen, &school)
 
-	fmt.Printf("내 동생은 %d살이야\n", n)
-	fmt.Printf("%d \n", arr)
-	fmt.Printf("%d %d", n, arr)
+	/**
+	공백과 개행으로 구분하여 입력한다.
+	*/
+	fmt.Scan(&name, &gen, &school)
+	fmt.Println("이름은 ", name, "성별은 ", gen, "학교는", school)
+
+	/**
+	포맷 지정자를 이용하여 개발자가 원하는 형태로 입력한다.
+	*/
+	var i, j int
+	fmt.Print("주민등록번호를 -를 이용해 입력하세요 :")
+	fmt.Scanf("%d-%d", &i, &j)
+
+	fmt.Printf("주민등록번호는 %d-%d\n", i, j)
+	fmt.Println("앞자리는", i)
+	fmt.Println("뒷자리는", j)
 }
